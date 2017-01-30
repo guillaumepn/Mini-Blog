e934231c99a1f91985536e4a3aface97b9322405
guillaumepn <guillaumepn@free.fr>
2017-01-31 00:27:03 +0100

    Fix du listing d'articles sur la home

--- header.php
+++ header.php
@@ -12,8 +12,6 @@ require_once("classAuthentification.php");
 	<meta charset="UTF-8">
 	<title>Mini-Blog</title>
 
-
-
 	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
      <script type="text/javascript">
 tinymce.init({
